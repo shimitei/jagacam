3afb23fa39d67be48bd2d8fca66bc9b939c097b9
Satoshi Shimizu <shimitei@gmail.com>
2016-08-17 12:29:00 +0900

    Add WebSocket connect

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res){
-  res.send('Hello world');
+app.get('/', function (req, res) {
+    res.send('Hello world');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+io.on('connection', function (socket) {
+    console.log('websocket connect: %s %s', socket.client.conn.remoteAddress, socket.id);
+
+    socket.on('disconnect', function () {
+        console.log('websocket disconnect: %s', socket.id);
+    });
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
